0a663b875d38357f1a58b2c33128f7377423f4af
Riko <eldoradozolotoe@gmail.com>
2022-11-29 02:28:09 +0700

    Version 0.1

---
+++ main.py
@@ -0,0 +1,83 @@
+import datetime
+import time
+import schedule
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from useful_funcs import form_dictionary, show_ticket_dict, get_date_for_url, generate_graph_from_dict
+
+exclude_list = ('Багаж', 'TOF', 'S7', 'UUD', '|', '+', 'Самый', 'Выбрать', 'цене', 'Без', 'Ред')
+
+
+def parse_to_files(url, result_file):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-blink-features=AutomationControlled")  # Disable web-driver mode
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                         " Chrome/96.0.4664.110 Safari/537.36")
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()), service_log_path='/dev/NULL')
+
+    driver.implicitly_wait(15)
+    try:
+        driver.get(url=url)
+        time.sleep(15)
+        element = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[3]/div/div/div/div/div/div[2]/div[2]')
+        parsed_text = element.text
+
+        with open('tmp.txt', 'w', encoding='utf-8') as output:
+            output.write(parsed_text)
+
+        with open('tmp.txt', 'r', encoding='utf-8') as file:
+            lst1 = file.readlines()
+
+        result = []
+        for q in range(len(lst1)):
+            rem = False
+            for word in lst1[q].split():
+                if word.strip() in exclude_list:
+                    rem = True
+            if not rem:
+                if '₽' in lst1[q] and q > 1:
+                    result.append('\n')
+                result.append(lst1[q])
+
+        with open(result_file, 'w', encoding='utf-8') as file:
+            file.writelines(result)
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+
+
+def start_parse():
+    current_time = str(datetime.datetime.now().time()).split('.')[0].split(':')
+    current_time = current_time[0] + '.' + current_time[1]
+    for i in range(1, 6):
+        date = get_date_for_url(i)
+        filename = f'{str(datetime.datetime.today()).split()[0]}_{current_time}_for_{date[:2]}.{date[2:]}.txt'
+        try:
+            parse_to_files(f'https://www.aviasales.ru/search/TOF{date}UUD1?request_source=expired_search', filename)
+        except:
+            pass
+        time.sleep(1)
+    # dictionary = form_dictionary()
+    # show_ticket_dict(dictionary)
+    # generate_graph_from_dict(dictionary)
+
+
+def main():
+    schedule.every().day.at('00:34').do(start_parse)
+    schedule.every().day.at('00:37').do(start_parse)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ useful_funcs.py
@@ -0,0 +1,77 @@
+import pathlib
+import datetime
+
+
+def show_ticket_dict(dictionary):
+    for key in dictionary:
+        tmp = dictionary[key]  # Массив на 10 массивов внутри
+        print(key, '\nБилет ', end='')
+        print(*tmp, sep='\nБилет ')
+        print()
+    print('*'*60)
+
+
+def refactor_files(list_of_files):
+    for file in list_of_files:
+        with open(file, 'r', encoding='utf-8') as f:
+            arr = f.readlines()
+        for i in range(len(arr)):
+            arr[i] = arr[i].replace(' ', '').replace(' ', '')
+        with open(file, 'w', encoding='utf-8') as f:
+            f.writelines(arr)
+
+
+def get_key_name(filename):
+    filename = str(filename)
+    filename = filename.split('_')
+    return str(filename[0] + '_at_' + filename[1] + '_for_' + filename[3][:5])
+
+
+def get_files_list():
+    currentDirectory = pathlib.Path('.')
+    list_of_files = []
+    for currentFile in currentDirectory.iterdir():
+        if str(currentFile).split('.')[-1] == 'txt' and str(currentFile).split('.')[0] != 'tmp':
+            list_of_files.append(currentFile)
+    return list_of_files
+
+
+def get_tickets(filename):
+    ticket_list = [[] for _ in range(10)]
+    counter = 0
+    with open(filename, 'r', encoding='utf-8') as file:
+        line = file.readline().strip('\n')
+        while line != '':
+            if line == '\n':
+                counter += 1
+            if '₽' in line:
+                ticket_list[counter].append(line.strip('\n'))  # Цена
+                flight_time = file.readline().strip('\n') # Время вылета
+                file.readline()
+                ticket_list[counter].append(file.readline().strip('\n') + ' ' + flight_time) # Дата вылета и время
+                ticket_list[counter].append(file.readline().strip('\n')) # Время в пути
+                flight_time = file.readline().strip('\n') # Время прибытия
+                file.readline()
+                ticket_list[counter].append(file.readline().strip('\n') + ' ' + flight_time) # Дата прибытия и время
+            line = file.readline()
+    return ticket_list
+
+
+def form_dictionary():
+    files_lst = get_files_list()
+    refactor_files(files_lst)
+    amount_of_keys = len(files_lst)
+    dictionary = dict()
+    for i in range(amount_of_keys):
+        dictionary[get_key_name(files_lst[i])] = get_tickets(files_lst[i])
+    return dictionary
+
+
+def get_date_for_url(offset_from_today: int):
+    ordinal_date = datetime.date.toordinal(datetime.date.today())
+    result = str(datetime.date.fromordinal(ordinal_date + offset_from_today)).split('-')
+    return result[2] + result[1]
+
+
+def generate_graph_from_dict(dictionary: dict):
+    pass
